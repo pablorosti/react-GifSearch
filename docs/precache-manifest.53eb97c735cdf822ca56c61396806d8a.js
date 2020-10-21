a6e5dbf82b3e635112af120590720d28863a4452
pablorosti <pablorostirollatenis@gmail.com>
2020-10-20 21:20:23 -0300

    docs agregado

---
+++ docs/precache-manifest.53eb97c735cdf822ca56c61396806d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0638adb39f7804118995fc25558294d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9110ac614d854e0d0d08",
+    "url": "/static/css/main.8f43ed28.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9110ac614d854e0d0d08",
+    "url": "/static/js/main.0f1290e9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
